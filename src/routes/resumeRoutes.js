4840674c35381d14bb2a3c4d657bcf689b1ed534
Tunji <54449991+Adetunjicode@users.noreply.github.com>
2021-08-18 11:48:01 +0100

    made some changes

--- src/routes/resumeRoutes.js
+++ src/routes/resumeRoutes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const resumeController = require('../controllers/resumeController');
 
-router.get('/resume', resumeController.getResumePage);
+router.route('/')
+    .get(resumeController.getResumePage);
+
 router.post('/contact-me', resumeController.contactMe);
 
 module.exports = router;
